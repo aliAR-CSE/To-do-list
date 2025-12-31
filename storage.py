7a0f6dbc95a3cf417ea3987b77b9af1b0b2c1019
Ali <aliabdulrazak128@gmail.com>
2025-12-30 21:33:58 -0500

    save_courses module

--- storage.py
+++ storage.py
@@ -1,2 +1,10 @@
 import json
-
+from course import Course
+file_name = "data.JSON"
+""""Converts the list of courses in a dictionary and imported into a JSON file"""
+def save_courses(file_name: str, courses: list[Course]):
+    data = []
+    for course in courses:
+        data.append(course.to_dict())
+    with open(file_name, "w", encoding = 'utf-8') as file:
+        json.dump(data, file, ensure_ascii = False, indent = 4)
