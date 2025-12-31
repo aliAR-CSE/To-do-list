f441a853277dacf27c0db5da7740128ef2dd3da6
Ali <aliabdulrazak128@gmail.com>
2025-12-30 22:10:37 -0500

    made it more clear that the file name is const

--- storage.py
+++ storage.py
@@ -1,10 +1,10 @@
 import json
 from course import Course
-file_name = "data.JSON"
+FILE_NAME = "data.JSON"
 """"Converts the list of courses in a dictionary and imported into a JSON file"""
-def save_courses(file_name: str, courses: list[Course]):
+def save_courses(FILE_NAME: str, courses: list[Course]):
     data = []
     for course in courses:
         data.append(course.to_dict())
-    with open(file_name, "w", encoding = 'utf-8') as file:
+    with open(FILE_NAME, "w", encoding = 'utf-8') as file:
         json.dump(data, file, ensure_ascii = False, indent = 4)
