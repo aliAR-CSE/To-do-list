ea71326a64939db9b3676b1249495f3bbb01206d
Ali <aliabdulrazak128@gmail.com>
2026-01-02 19:45:47 -0500

    created central widget

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, 
-                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+                             QWidget, QVBoxLayout, QHBoxLayout, 
+                             QGridLayout, QDesktopWidget)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Acedemic Planner")
+        self.initUI()
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        vbox = QVBoxLayout()
+
+        central_widget.setLayout(vbox)
+    
 
 def main():
     app = QApplication(sys.argv)
