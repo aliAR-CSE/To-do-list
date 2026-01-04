f7ca778a11be603c13ba1f4a8d70dfe311a8f923
Ali <aliabdulrazak128@gmail.com>
2026-01-04 13:15:43 -0500

    drop down menu added and self centering window

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, 
                              QWidget, QVBoxLayout, QHBoxLayout, 
-                             QGridLayout, QDesktopWidget)
+                             QGridLayout, QDesktopWidget, QTreeWidgetItem,
+                             QTreeWidget)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Acedemic Planner")
+        self.resize(900, 600)
+        self.center()
         self.initUI()
 
     def initUI(self):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
+        self.center()
 
-        vbox = QVBoxLayout()
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
 
-        central_widget.setLayout(vbox)
+        drop_down = QTreeWidget()
+        drop_down.setColumnCount(4)
+        layout.addWidget(drop_down)
+        drop_down.setHeaderLabels(['Name', 'Type', 'Weight', 'Grade'])
     
-
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
